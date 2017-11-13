ae91e1baea851ce88fc924dbbd4db279604a8bfe
diego.galico <diego.galico@schibsted.com>
2017-11-13 19:04:54 +0100

    String calculator rule 4

---
+++ app/src/main/java/com/example/diegogalico/karumi/model/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.diegogalico.karumi.model;
+
+/**
+ * Created by diego.galico on 13/11/2017.
+ */
+
+public class Calculator {
+}
---
+++ app/src/test/java/com/example/diegogalico/karumi/model/CalculatorTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by diego.galico on 13/11/2017.
+ */
+public class CalculatorTest {
+
+}
